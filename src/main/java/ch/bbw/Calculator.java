75baabcd87f819d42bca69f325f296c7d179fedc
Davide Mastruzzo <Davide.Mastruzzo@netcetera.com>
2018-12-18 12:27:01 +0100

    implement Calculator + sum function

---
+++ src/main/java/ch/bbw/Calculator.java
@@ -0,0 +1,14 @@
+package ch.bbw;
+
+/**
+ * Calculator
+ * - kann  zwei Zahlen addieren
+ *
+ * @author Davide Mastruzzo
+ */
+public class Calculator {
+
+    public int summe(int summand1, int summand2) {
+        return summand1 + summand2;
+    }
+}
